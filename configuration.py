0fa6acc19fff1deb856d650ae60311bb7dfa091d
yuanyexi <blaizh@gmail.com>
2021-03-18 15:35:02 +0800

    request 2021 Jan-Feb demand data

--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
-START_DATE = '2018-01-01'
-END_DATE = '2018-12-31'
+START_DATE = '2021-01-01'
+END_DATE = '2021-02-28'
 MARKETS = ['PH', 'ID', 'MY', 'SG', 'TH', 'AU', 'NZ', 'IN', 'JP' ,'CN', 'TW', 'KR']
 METRIC_TYPE = 'dexpercapita'
 INTERVAL = 'overall'
